fd831507f6be91b765abf301d91eaf70d6929a17
VaMpiRe <jishnuvamp@gmail.com>
2018-04-11 10:49:49 +0530

    generated controllers

---
+++ controllers/UserController.php
@@ -0,0 +1,165 @@
+<?php
+
+namespace app\controllers;
+
+use Yii;
+use yii\filters\AccessControl;
+use yii\web\Controller;
+use yii\filters\VerbFilter;
+use app\models\LoginForm;
+use app\models\ContactForm;
+use app\models\User;
+use app\models\Staff;
+
+class UserController extends Controller
+{
+	public $defaultAction = 'dashboard';
+    /**
+     * @inheritdoc
+     */
+    public function behaviors()
+    {
+        return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'only' => ['logout','dashboard'],
+                'rules' => [
+                    [
+                        'actions' => ['logout','dashboard'],
+                        'allow' => true,
+                        'roles' => ['@'],
+                    ],
+                ],
+            ],
+            'verbs' => [
+                'class' => VerbFilter::className(),
+                'actions' => [
+                    'logout' => ['post'],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * @inheritdoc
+     */
+    public function actions()
+    {
+        return [
+            'error' => [
+                'class' => 'yii\web\ErrorAction',
+            ],
+            'captcha' => [
+                'class' => 'yii\captcha\CaptchaAction',
+                'fixedVerifyCode' => YII_ENV_TEST ? 'testme' : null,
+            ],
+        ];
+    }
+    
+    
+    public function actionDashboard()
+    {
+    	switch(Yii::$app->user->identity->type){
+    		case User::USER_TYPE_ADMIN:
+    		{
+    			break;
+    		}
+    		case User::USER_TYPE_STAFF:
+    		{     			
+    			$staff = Yii::$app->user->identity->staff;
+    			if(!$staff->dataEntered()){
+    				$this->redirect('/staff/registration/form');
+    			}
+    				
+    		}
+    		break;
+    		case User::USER_TYPE_STUDENT:
+    		{
+    			
+    		}
+    		break;
+    		case User::USER_TYPE_PARENT:
+    		{
+    			
+    		} 
+    		break; 
+    		default :
+    		{
+    			
+    		}  		
+    	}
+        return $this->render('index');
+    }
+
+    /**
+     * Displays homepage.
+     *
+     * @return string
+     */
+    public function actionIndex()
+    {
+        return $this->render('index');
+    }
+
+    /**
+     * Login action.
+     *
+     * @return string
+     */
+    public function actionLogin()
+    {
+        if (!Yii::$app->user->isGuest) {
+            return $this->goHome();
+        }
+
+        $model = new LoginForm();
+        //$hash = Yii::$app->getSecurity()->generatePasswordHash('01fcd5');
+        //echo $hash;die;
+        if ($model->load(Yii::$app->request->post()) && $model->login()) {
+            return $this->goBack();
+        }
+        return $this->render('login', [
+            'model' => $model,
+        ]);
+    }
+
+    /**
+     * Logout action.
+     *
+     * @return string
+     */
+    public function actionLogout()
+    {
+        Yii::$app->user->logout();
+
+        return $this->goHome();
+    }
+
+    /**
+     * Displays contact page.
+     *
+     * @return string
+     */
+    public function actionContact()
+    {
+        $model = new ContactForm();
+        if ($model->load(Yii::$app->request->post()) && $model->contact(Yii::$app->params['adminEmail'])) {
+            Yii::$app->session->setFlash('contactFormSubmitted');
+
+            return $this->refresh();
+        }
+        return $this->render('contact', [
+            'model' => $model,
+        ]);
+    }
+
+    /**
+     * Displays about page.
+     *
+     * @return string
+     */
+    public function actionAbout()
+    {
+        return $this->render('about');
+    }
+}
